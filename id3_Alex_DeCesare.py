c1a8a50875d402577507a430634a0448bd8df0a8
alex decesare <alexdecesare@hotmail.com>
2020-10-23 09:08:01 -0400

    added the final code which parses the mp3 file and holds its data and made a output string which shows the data to the user via command line

--- id3_Alex_DeCesare.py
+++ id3_Alex_DeCesare.py
@@ -16,16 +16,37 @@ def parse_mp3_file():
    
     mp3_file = get_file(file_location)
     mp3_file.seek(-128,2)
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> added the final code which parses the mp3 file and holds its data and made a output string which shows the data to the user via command line
     if str(mp3_file.read(3), 'utf-8') == 'TAG':
 
         mp3_contents = {
 
                     'title' : str(mp3_file.read(30), 'utf-8'),
+<<<<<<< HEAD
                     'artist' : str(mp3_file.read(30), 'utf-8')
 
                 }
         print(mp3_contents)
+=======
+                    'artist' : str(mp3_file.read(30), 'utf-8'),
+                    'album' : str(mp3_file.read(30), 'utf-8'),
+                    'year' : str(mp3_file.read(4), 'utf-8'),
+                    'comment' : str(mp3_file.read(28), 'utf-8')
+                        
+                }
+        mp3_output = 'Title: ' + mp3_contents['title'] + '\n' \
+        + 'Artist: ' + mp3_contents['artist'] + '\n' \
+        + 'Album: ' + mp3_contents['album'] + '\n' \
+        + 'Year: ' + mp3_contents['year'] + '\n' \
+        + 'Comment: ' + mp3_contents['comment']
+        
+        print(mp3_output)
+        mp3_file.close()
+>>>>>>> added the final code which parses the mp3 file and holds its data and made a output string which shows the data to the user via command line
 
     else:
         print('No ID3 information')
